ab5bceab9b2efa6282e6421e860188f8b8b7a83f
Lotus <{34827934@qq.com}>
2015-07-22 09:39:05 +0800

    Initial Commit

---
+++ QMClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QMClient
+//
+//  Created by Lotus on 15/7/22.
+//  Copyright (c) 2015年 Lotus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
